3da7d4b2d3eed7a7ad859d05a8870f5186dcb570
Caroline-Patrick <carolinespatrick@gmail.com>
2023-03-30 21:24:17 -0500

    signin hooked up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import axios from 'axios';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
+import { Signin } from './components/Signin';
 
 function App() {
+  const [token, setToken] =useState("");
+
   useEffect(()=>{
-    axios.get("http://localhost:5000/users").then((response)=>{console.log(response)})
-  }, [])
+    axios.get("http://localhost:5000/todos", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then((response)=>{console.log(response)})
+  }, [token]);
+
+  useEffect(()=>{
+    console.log("token", token)
+  }, [token])
 
   return (
     <div className="App">
-    Hello World
+    <Signin setToken={setToken}/>
     </div>
   );
 }
---
+++ src/components/Signin.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+export const Signin = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] =useState("");
+ 
+
+    useEffect(()=>{
+      console.log({password})
+    }, [password]);
+
+    useEffect(()=>{
+        console.log({email})
+      }, [email]);
+
+
+    return(
+        <>
+        <form 
+            className='form' 
+                onSubmit={(e)=>{
+                    e.preventDefault()
+
+                    axios
+                    .post("http://localhost:5000/signin", {
+                      email,
+                      password,
+                    })
+                    .then((response) => props.setToken(response.data.token))
+                    .catch((error) => {
+                      if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.error("Error data: ", error.response.data);
+                        console.error("Error status: ", error.response.status);
+                        console.error("Error headers: ", error.response.headers);
+                      } else if (error.request) {
+                        // The request was made but no response was received
+                        console.error("Error request: ", error.request);
+                      } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.error("Error message: ", error.message);
+                      }
+                      console.error("Error config: ", error.config);
+                    });
+                }}>
+
+            
+            <label className='label'>
+                Email: 
+                <input 
+                    className="input" 
+                    type="email"
+                    value={email} 
+                    onChange={(e)=>setEmail(e.target.value)}/>
+            </label>
+            
+            <label className='label'>
+                Password:
+                <input 
+                    className="input" 
+                    type="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    />
+            </label>
+            <input type="submit" value="Sign in"/>
+        </form>
+        </>
+    )
+}
